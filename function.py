b323b24daa7d40730b5700646341ac4e5e55ac86
Sougata Das <sougata19582001@gmail.com>
2022-07-25 01:17:27 +0530

    add edabit questions code

---
+++ function.py
@@ -0,0 +1,383 @@
+import re
+
+
+
+def ways_to_climb(n, first=0, second=1):
+    if n <= 1:
+        return first + second
+    return ways_to_climb(n - 1, second, first + second)
+
+
+def path_finder(lst):
+    n = len(lst)-1
+    path_sum = 0
+    i, j = 0, 0
+    while i < n or j < n:
+        go_down, go_right = False, False
+        path_sum += lst[i][j]
+        # Restrict the path if i or j has reached to the limit.
+        if i == n:
+            go_right = True
+        elif j == n:
+            go_down = True
+        elif lst[i][j+1] == lst[i+1][j]:
+            # go down
+            go_down = True
+        elif lst[i][j+1] > lst[i+1][j]:
+            # go right
+            go_right = True
+        elif lst[i][j+1] < lst[i+1][j]:
+            # go down
+            go_down = True
+
+        # giving the movement after the successfull decision is made.
+        if go_down:
+            i += 1
+
+        if go_right:
+            j += 1
+
+    # summing up the last value as the while loop end before calculating it.
+    return path_sum + lst[i][j]
+
+
+def can_see_stage(seats):
+    col = len(seats[0])-1
+    row = len(seats)-1
+
+    for i in range(col):
+        for j in range(row):
+            if seats[j][i] >= seats[j+1][i]:
+                return False
+    return True
+
+
+def find_pattern(dct_, guess):
+    output = []
+    for phrase in dct_:
+        refined_phrase = dct_[phrase].replace('%d%', '')
+        if guess.casefold() in refined_phrase.casefold():
+            output.append(f"{phrase}={guess}")
+        else:
+            output.append(f"{phrase}=None")
+    return output
+
+
+def freed_prisoners(prison):
+    if prison[0] == 0:
+        return 0
+    freed = 0
+    opposite_prison = [1 if x == 0 else 0 for x in prison]
+    main_prison = prison
+    i = 0
+    while i < len(main_prison):
+        if main_prison[i] == 1:
+            freed += 1
+            main_prison = opposite_prison if main_prison is prison else prison
+        i += 1
+    return freed
+
+
+def advanced_sort(lst: list):
+    dct = {}
+    for i in lst:
+        dct[i] = dct.get(i, 0) + 1
+
+    output = []
+    for num in dct.items():
+        temp = []
+        for i in range(dct[num]):
+            temp.append(num)
+        output.append(temp)
+
+    return output
+
+
+def dakti(sentence: str):
+    # removing special characters %^&*?><,.
+    sentence = re.sub('[^a-zA-Z0-9 -]', '', sentence).strip()
+    # sorting the string with respect to the number in the word
+    arr = sorted(sentence.split(), key=lambda x: int(
+        "".join([n for n in x if n.isdigit()])))
+    # array to string
+    sentence = " ".join(arr)
+    # remove the numbers from the words in sentence
+    sentence = re.sub('[^a-zA-Z -]', '', sentence)
+    print(sentence)
+
+
+# Binary search revision.
+def binary_search(lst, high, low, x):
+    if high >= low:
+        mid = (high + low) // 2
+
+        if lst[mid] == x:
+            return True
+        elif lst[mid] > x:
+            return binary_search(lst, mid-1, low, x)
+        else:
+            return binary_search(lst, high, mid + 1, x)
+    else:
+        return False
+
+
+# lst = [2, 3, 4, 10, 40]
+# print(binary_search(lst, len(lst)-1, 0, 9))
+
+
+def collect(code, n, l=0, r=None, lst=None):
+    if lst is None:
+        lst = []
+        r = n
+
+    # if the string length is not multiple of the n
+    # donot add the remaining characters.
+    if r > len(code):
+        lst.sort()
+        return lst
+
+    lst.append(code[l:r])
+    return collect(code, n, r, r+n, lst)
+
+
+def identify(*cube):
+    columns = []
+    missing = 0
+    for part in cube:
+        columns.append(len(part))
+    rows = len(cube)
+    columns.sort()
+    columns = columns[::-1]
+
+    max_col = columns[0]
+    for i in range(1, len(columns)):
+        if max_col - columns[i] != 0:
+            missing += max_col - columns[i]
+
+    if missing > 0:
+        return f"missing {missing}"
+
+    if rows != max_col:
+        return "non-full"
+
+    return "full"
+
+
+def num_split(num):
+    # number is negetive negate the number.
+    # split the number
+    # number x 10 ** n (n = 0, 1, 2, 3...)
+    # the the number to the list
+    # return the reverse of the list.
+    if not num:
+        return [0]
+
+    is_negetive = False
+    if num < 0:
+        is_negetive = True
+        num = -num
+
+    place = 0
+    split_list = []
+    while num > 0:
+        remainder = num % 10
+        if is_negetive:
+            remainder *= -1
+        split_list.append(remainder*(10**place))
+        place += 1
+        num //= 10
+
+    return split_list[::-1]
+
+
+def max_product(lst):
+    lst.sort()
+    l = 0
+    r = len(lst)-1
+    max_prod = -10000
+    while l < len(lst)-2:
+        for i in range(l+1, r):
+            max_prod = max(max_prod, lst[l]*lst[i]*lst[r])
+        l += 1
+    return max_prod
+
+
+def min_product(lst):
+    lst.sort()
+    lst = lst[::-1]
+    l = 0
+    r = len(lst) - 1
+    min_prod = 100000
+    while l < len(lst)-2:
+        for i in range(l+1, r):
+            min_prod = min(min_prod, lst[l]*lst[i]*lst[r])
+        l += 1
+    return min_prod
+
+
+def find_and_remove(p_dct):
+    # traverse through the list.
+    # go to the nested dct
+    # check every item'code value for digit or not
+    # create a list to contain the items to be removed.
+    # when the iteration is over then iterate over the list and delete the item
+    # from the dct using the temp dct created.
+
+    for nested_dct in p_dct:
+        remove_items = []
+        temp = p_dct[nested_dct]
+        # print(nested_dct ,temp)
+        for item in temp:
+            if not temp[item].isdigit():
+                remove_items.append(item)
+        # print(remove_items)
+        for remove_item in remove_items:
+            temp.pop(remove_item)
+
+    return p_dct
+
+
+def connell_sequence(start, end, n):
+    # start is the starting row
+    # end is the end row
+    # n is the number whose index in final list is to be found.
+    sequence_start = False
+    sequence = []
+    num = 1
+    # row count
+    for row in range(1, end+1):
+        i = 0  # for indexing of each row.
+
+        # for checking if the row is even or not
+        even_row = False
+        if row % 2 == 0:
+            even_row = True
+        # to check if the start row is reached ,
+        # if reached then start appending the number in the resultant array.
+        if row == start:
+            sequence_start = True
+
+        while i < row:
+            if even_row:
+                if num % 2 == 0:  # check if the num is even, according to the row number.
+                    if sequence_start:  # if the start is reached then start appending the number.
+                        sequence.append(num)
+                    i += 1  # incrementing the i only when number is found.
+            else:
+                if num % 2 != 0:  # check if the num is odd, according to the row number.
+                    if sequence_start:  # if the start is reached then start appending the number.
+                        sequence.append(num)
+                    i += 1  # incrementing the i only when number is found.
+
+            num += 1  # incrementing the num
+
+    # print(sequence)
+    if n in sequence:
+        return sequence.index(n)
+    else:
+        return "Not Found!"
+  
+  
+#  Function to print permutations of string 
+# This function takes three parameters: 
+# 1. String 
+# 2. Starting index of the string 
+# 3. Ending index of the string. 
+def permute(a, l, r): 
+    if l==r: 
+        print (''.join(a))
+    else: 
+        for i in range(l,r): 
+            a[l], a[i] = a[i], a[l] 
+            permute(a, l+1, r)
+            # var = None 
+            a[l], a[i] = a[i], a[l] # backtrack 
+  
+# Driver program to test the above function 
+# string = "ABC"
+# n = len(string) 
+# a = list(string) 
+# permute(a, 0, n)
+
+# def factorial(n):
+#     if n == 1:
+#         return 1
+#     return factorial(n-1) * n
+
+# def permute(a, l, r, output=None):
+#     if output is None:
+#         output = []
+
+#     if l == r:
+#         output.append("".join(a))
+#     else:
+#         for i in range(l, r):
+#             a[l], a[i] = a[i], a[l]
+#             permute(a, l + 1, r, output=output)
+#             # a[l], a[i] = a[i], a[l]
+    
+    
+#     return output
+
+
+
+# string = 'ab'
+# array = [_ for _ in string]
+# n = len(string)
+
+# print(permute(array, 0, n))
+
+
+def advanced_sort(lst):
+    if len(lst) < 2:
+        return [lst]
+
+    lst.sort()
+    left = 0
+    ans = []
+    while left < len(lst)-1:
+
+        right = left + 1
+
+        while lst[right] == lst[right-1] and right < len(lst)-1:
+            right += 1
+
+        if right == len(lst)-1 and lst[right] == lst[right-1]:
+            # if the last element is equal to second last then
+            # append all the remaining elements
+            ans.append(lst[left:])
+            break
+
+        if right == len(lst)-1 and lst[right] != lst[right-1]:
+            # if the last element is not equal to second last then
+            # first append the lst[left->right] and the last remaining element
+            ans.append(lst[left:right])
+            ans.append([lst[-1]])
+            break
+
+        ans.append(lst[left:right])
+
+
+
+        left = right
+        
+
+    return ans
+
+def guess_score(code, guess):
+    black = 0
+    white = 0
+    for index, char in enumerate(code):
+        if char == guess[index]:
+            black += 1
+        elif char in guess:
+            white += 1
+
+    return {"black":black,"white":white}
+
+print(guess_score("1423", "5678"), {"black": 0, "white": 0})
+print(guess_score("1423", "2222"), {"black": 1, "white": 0})
+print(guess_score("1423", "1234"), {"black": 1, "white": 3})
+print(guess_score("1423", "2211"), {"black": 0, "white": 2})
+print(guess_score("2928", "7722"), {"black": 1, "white": 1})
+print(guess_score("4845", "6446"), {"black": 1, "white": 1})
---
+++ main.py
@@ -0,0 +1,287 @@
+from function import ways_to_climb
+from function import path_finder
+from function import can_see_stage
+from function import find_pattern
+from function import freed_prisoners
+from function import advanced_sort
+from function import dakti
+from function import collect
+from function import identify
+from function import num_split
+from function import max_product
+from function import min_product
+from function import find_and_remove
+from function import connell_sequence
+
+if __name__ == '__main__':
+    for i in range(100):
+        print(ways_to_climb(i))
+
+    print("*"*40)
+
+    lst = [
+        [1, 0, 3],
+        [0, 0, 6],
+        [0, 8, 9]
+    ]
+    lst1 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [0, 8, 9]
+    ]
+    print(path_finder(lst))
+    print(path_finder(lst1))
+
+    print('*'*40)
+
+    print(can_see_stage([
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]))
+    print(can_see_stage([
+        [0, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2]
+    ]))
+    print(can_see_stage([
+        [2, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2]
+    ]))
+    print(can_see_stage([
+        [1, 0, 0],
+        [1, 1, 1],
+        [2, 2, 2]
+    ]))
+    print(can_see_stage([
+        [1, 2, 3, 2, 1, 1],
+        [2, 4, 4, 3, 2, 2],
+        [5, 5, 5, 5, 4, 4],
+        [6, 6, 7, 6, 5, 5]
+    ]))
+    print(can_see_stage([[1, 2, 3, 2, 1, 1],
+                         [2, 4, 4, 3, 2, 2],
+                         [5, 5, 5, 10, 4, 4],
+                         [6, 6, 7, 6, 5, 5]]))
+
+    print('*'*40)
+
+    print(find_pattern({
+        "Phrase1": "COVID-19 is no good",
+        "Phrase2": "COVID-18 is no good",
+        "Phrase3": "COVID-17 is no good"
+    }, "COVID-19"))
+    print(find_pattern({
+        "Phrase1": "Edabit is great",
+        "Phrase2": "Edabit is very great",
+        "Phrase3": "Edabit is really great"
+    }, "really")
+    )
+    print(find_pattern({
+        "Phrase1": 'Made in China',
+        "Phrase2": 'Made in Brazil',
+        "Phrase3": 'Made in America',
+        "Phrase4": 'Made in Colombia',
+
+    }, 'Jhonson'))
+    print(find_pattern({
+        "Phrase1": 'Made12 in China',
+        "Phrase2": 'Made in Brazil',
+        "Phrase3": '32Made in America',
+        "Phrase4": 'Made in Colombia',
+
+    }, 'Made'))
+
+    print('*'*40)
+
+    print(freed_prisoners([1, 1, 0, 0, 0, 1, 0]))
+    print(freed_prisoners([1, 1, 1]))
+    print(freed_prisoners([0, 0, 0]))
+    print(freed_prisoners([0, 1, 1, 1]))
+    print(freed_prisoners([1, 0, 1, 0, 1, 0]))
+    print(freed_prisoners([1, 1, 1, 0, 0, 0]))
+    print(freed_prisoners([1, 0, 0, 0, 0, 0, 0]))
+
+    print("*"*40)
+
+    print(advanced_sort([2, 1, 2, 1]))
+    print(advanced_sort([5, 4, 5, 5, 4, 3]))
+    print(advanced_sort(["b", "a", "b", "a", "c"]))
+    print(advanced_sort([3, 2, 1, 3, 2, 1]))
+    print(advanced_sort([5, 5, 4, 3, 4, 4]))
+    print(advanced_sort([80, 80, 4, 60, 60, 3]))
+    print(advanced_sort(['c', 'c', 'b', 'c', 'b', 1, 1]))
+    print(advanced_sort([1234, 1235, 1234, 1235, 1236, 1235]))
+    print(advanced_sort(['1234', '1235', '1234', '1235', '1236', '1235']))
+
+    print("*"*40)
+
+    dakti("i2s th1s a3 t4est    ")
+    dakti("yo2u cr3azy a1re ?  ")
+    dakti("en5tere y2a bab1y y3o 4me s6e not7a cuand8o 9me-ves")
+    dakti('h4as don2de ah1i n3o ll5egado q7ue 8yo te9-llevare s6abes')
+
+    print('*'*40)
+
+    str_vector = [
+        "intercontinentalisationalism", "strengths", "pneumonoultramicroscopicsilicovolcanoconiosis",
+        "lexicographically", "anesthesiologists", "subdermatoglyphic", "sesquipedalianism",
+        "recollection", "pseudopseudohypoparathyroidism", "floccinaucinihilipilification",
+        "antidisestablishmentarianism", "supercalifragilisticexpialidocious", "incomprehensibilities",
+        "astrophysicists", "honorificabilitudinitatibus", "unimaginatively", "euouae", "tsktsk",
+        "uncopyrightable"
+    ]
+    num_vector = [6, 3, 15, 4, 6, 6, 6, 3, 7, 2, 5, 3, 9, 4, 12, 8, 7, 6, 11]
+    test_cases = list(zip(str_vector, num_vector))
+
+    for s, n in test_cases:
+        print(collect(s, n))
+
+    print('*'*40)
+
+    print(identify(
+    ["O", "O", "O"],
+    ["O", "O", "O"],
+    ["O", "O", "O"]
+    ))
+    print(identify(
+        ["O", "O"],
+        ["O", "O", "O"]
+    ))
+    print(identify(
+        ["O", "O"],
+        ["O", "O"],
+        ["O", "O", "O"]
+    ))
+
+    print(identify(
+        ["O", "O", "O", "O"],
+        ["O", "O", "O", "O"],
+        ["O", "O", "O", "O"],
+        ["O", "O", "O", "O"]
+    ))
+    print(identify(
+        ["O", "O", "O", "O"],
+        ["O", "O", "O", "O"],
+        ["O", "O", "O"],
+        ["O", "O", "O", "O"]
+    ))
+    print(identify(
+        ["O", "O"],
+        ["O", "O"]
+    ))
+    print(identify(
+        ["O", "O"],
+        ["O"]
+    ))
+
+    print("*"*40)
+
+    print(num_split(39))
+    print(num_split(100))
+    print(num_split(-434))
+    print(num_split(3929))
+    print(num_split(10293))
+    print(num_split(900))
+    print(num_split(-100))
+
+    print("*"*40)
+
+    print(max_product([1, -1, 1]), -1)
+    print(max_product([1, -1, 1, 1]), 1)
+    print(max_product([-8, -9, 1, 2, 7]), 504)
+    print(max_product([-8, 1, 2, 7, 9]), 126)
+    print(max_product([1, 1, 5, 1, 1, -10, -1]), 50)
+    print(max_product([-8, -7, -6, -5]), -210)
+    print(max_product([-8, -7, -6, -5, 1]), 56)
+    print(max_product([1, 0, 1, 0, 0]), 0)
+    print(max_product([-5, 1, 10, 0, 0]), 0)
+    print(max_product([-5, -1, -1, 0, 0]), 0)
+    print(max_product([-5, 1, -1, 0, 0]), 5)
+    print(max_product([-5, -3, -1, 0, 4]), 60)
+    print(max_product([5, 3, -1, 0, -4, 7, 7, 9]), 441)
+
+    print("*"*40)
+
+    print(min_product([1, -1, 1]), -1)
+    print(min_product([1, -1, 1, 1]), -1)
+    print(min_product([-8, -9, 1, 2, 7]), -126)
+    print(min_product([-8, 1, 2, 7, 9]), -504)
+    print(min_product([1, 1, 5, 1, 1, -10, -1]), -50)
+    print(min_product([-8, -7, -6, -5]), -336)
+    print(min_product([-8, -7, -6, -5, 1]), -336)
+    print(min_product([1, 0, 1, 0, 0]), 0)
+    print(min_product([-5, 1, 10, 0, 0]), -50)
+    print(min_product([-5, -1, -1, 0, 0]), -5)
+    print(min_product([-5, 1, -1, 0, 0]), 0)
+    print(min_product([-5, -3, -1, 0, 4]), -15)
+    print(min_product([5, 3, -1, 0, -4, 7, 7, 9]), -252)
+
+    print("*"*40)
+
+    print(find_and_remove({
+    "workshop": {
+      "bedsheets": "2000",
+      "working": "v0g89t7t",
+      "pen": "370",
+      "movies": "wo1a3d5d",
+    },
+}))
+
+print(find_and_remove({
+  "bedroom": {
+    "slippers": "10000",
+    "piano": "5500",
+    "call": "vet",
+    "travel": "world",
+  },
+}))
+
+print(find_and_remove({
+  'bedroom': {
+    'slippers': "10000",
+    'piano': "5500",
+    'call': "vet",
+    'travel': "world",
+  },
+}))
+
+print(find_and_remove({
+  'kitchen': {
+    "gold spoons": "900",
+    'piano': "550",
+    'notes': "ga0r76ba22g4e",
+  },
+  'cellar': {
+    'reminder': "dog",
+    'bottle': "750",
+  },
+}))
+
+print("*"*40)
+
+print(connell_sequence(1, 3, 4), 2, "Example #1")
+print(connell_sequence(2, 3, 4), 1, "Example #2")
+print(connell_sequence(4, 5, 22), "Not Found", "Example #3")
+print(connell_sequence(1, 1, 1), 0)
+print(connell_sequence(1, 1, 0), "Not Found")
+print(connell_sequence(1, 100, 100), 54)
+print(connell_sequence(4, 5, 77), "Not Found")
+print(connell_sequence(11, 22, 300), 103)
+print(connell_sequence(30, 32, 974), 67)
+print(connell_sequence(13, 88, 300), 80)
+print(connell_sequence(1, 1000, 8000), 4044)
+print(connell_sequence(111, 1000, 8000), "Not Found")
+# print(connell_sequence(10000, 11000, 120999810), 10510404) takes 7-8 seconds
+
+print("*"*40)
+
+print(advanced_sort([1,2,1,2]) , [[1,1],[2,2]])
+print(advanced_sort([2,1,2,1]) , [[2,2],[1,1]])
+print(advanced_sort([3,2,1,3,2,1]) , [[3,3],[2,2],[1,1]])
+print(advanced_sort([5,5,4,3,4,4]) , [[5,5],[4,4,4],[3]])
+print(advanced_sort([80,80,4,60,60,3]),[[80,80],[4],[60,60],[3]])
+print(advanced_sort(['c','c','b','c','b']),[['c','c','c'],['b','b']])
+print(advanced_sort([1234, 1235, 1234, 1235, 1236, 1235]),[[1234, 1234],[1235, 1235, 1235],[1236]])
+print(advanced_sort(['1234', '1235', '1234', '1235', '1236', '1235']),[['1234', '1234'],['1235', '1235', '1235'],['1236']])
